e731ae80c236cdf000ac1e0f4a83365a1c7c25e6
Dtesch9 <alucard.s8123@gmail.com>
2020-03-24 15:13:36 -0300

    init

---
+++ backend/src/app/controllers/OngController.js
@@ -0,0 +1,30 @@
+import crypto from 'crypto';
+
+import db from '../../database';
+
+class OngController {
+  async index(req, res) {
+    const ongs = await db('ongs').select('*');
+
+    return res.json(ongs);
+  }
+
+  async store(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await db('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    });
+
+    return res.status(200).json({ id });
+  }
+}
+
+export default new OngController();

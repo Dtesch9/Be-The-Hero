41c2a2b0931cf0ca4dcefaa47bd68cb2071c1898
Dtesch9 <alucard.s8123@gmail.com>
2020-03-26 11:39:56 -0300

    init

---
+++ mobile/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StatusBar } from 'react-native';
+
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>Be The Hero</Text>
+    </>
+  );
+};
+
